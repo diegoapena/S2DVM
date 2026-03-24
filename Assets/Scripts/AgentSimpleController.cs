04a4ff67135adb2f7d848b0755eaa2d4db2f7fa2
Diego Eduardo Apeña De La Cruz <diego.apena@tecsup.edu.pe>
2026-03-24 10:47:10 -0500

    clase

---
+++ Assets/Scripts/AgentSimpleController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class AgentSimpleController : MonoBehaviour
+{
+    public Transform Target; 
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent != null) 
+        {
+            agent.SetDestination(Target.position);
+        }
+    }
+}
